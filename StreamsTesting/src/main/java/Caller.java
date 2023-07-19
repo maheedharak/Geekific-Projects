01e7682786367afe96b42e34d9d9598639f480cd
maheedharak <115575926+maheedharak@users.noreply.github.com>
2023-07-19 14:00:37 +0200

    19July 2022 practice

--- StreamsTesting/src/main/java/Caller.java
+++ StreamsTesting/src/main/java/Caller.java
@@ -5,11 +5,11 @@ import java.util.Optional;
 public class Caller {
     public static void main(String[] args){
         QID_2_2023 qid22023 = new QID_2_2023();
-   //     qid22023.streamIntegers();
+      qid22023.streamIntegers();
         QID_Q2_1762 qidq21762 = new QID_Q2_1762();
-   //     qidq21762.streamNames();
+       qidq21762.streamNames();
         QID_2_1787 qid21787 = new QID_2_1787();
-    //    qid21787.flatMapStream();
+      qid21787.flatMapStream();
         QID_2_1738 qid21738 = new QID_2_1738();
         qid21738.sumIntStream();
         qid21738.comparatorStream();
@@ -21,14 +21,15 @@ public class Caller {
         grade1.orElse("UNKNOWN");
         grade2.ifPresent(System.out::println);
         grade2.orElse("Empty");
-        QID_2_1809 qid_2_1809 = new QID_2_1809();
-        qid_2_1809.averageCalculator();
+         QID_2_1809.averageCalculator();
         QID_2_1846 qid21846 = new QID_2_1846();
         qid21846.mapper();
         QID_2_1847 qid21847 = new QID_2_1847();
         qid21847.mapTesting();
         QID_2_1810 qid21810 = new QID_2_1810();
         qid21810.filterWithMap();
+        QID_2_1849 qid21849 = new QID_2_1849();
+        qid21849.optionalTesting();
     }
 
 }
